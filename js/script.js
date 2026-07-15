918955537338c440ff503a784a22a634c9f9b1dd
FrancoBurgos123 <franco_123burgos@hotmail.com>
2026-07-15 12:59:57 -0300

    cambios en js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 window.onload = function () {
     alert("¡Bienvenido a Red Asistencia Solidaria!");
 }
+
+const boton = document.getElementById("modoOscuro");
+
+boton.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+});
